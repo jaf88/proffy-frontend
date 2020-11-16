f03b170994055128db3c29a7b424b83a79eac944
jaf88 <jaf.alves8@gmail.com>
2020-11-16 19:23:52 -0300

    feat: interfaces

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,36 +1,53 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number,
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
   return (
     <article className="teacher-item">
     <header>
-      <img src="https://avatars3.githubusercontent.com/u/17249975?s=460&u=3197a6ae0fd730cb9e027c0add6d50c9128880cd&v=4" alt="avatar"/>
+      <img src={teacher.avatar} alt={teacher.name}/>
       <div>
-        <strong> Joaquim Alves </strong>
-        <span> Web Programming</span>
+        <strong> {teacher.name} </strong>
+        <span> {teacher.subject}</span>
       </div>
     </header>
 
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing.
-      <br/> <br/>
-      elit. Modi corrupti soluta praesentium, architecto 
-      ab aliquid? Odio facere amet a iusto magni autem 
-    </p>
+    <p> {teacher.bio} </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong> R$ 80,00 </strong>
+          <strong> R$ {teacher.cost} </strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank" 
+          onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   )
